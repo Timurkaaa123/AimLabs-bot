e8e2d844c1484f4711254d27dae0fe58be96b85d
Timurkaaa <timurzarovskiy@gmail.com>
2025-07-31 21:28:08 +0300

    feat(aiming) create choose_target.py
    Script for selecting the nearest target

---
+++ src/bot/modules/aiming/choose_target.py
@@ -0,0 +1,36 @@
+import numpy
+
+def choose_target(targets, frame):
+	# Find center
+	height, width = frame.shape[:2]
+	center_x = width // 2
+	center_y = height // 2
+
+
+	# Coordinats
+	boxes = targets.boxes.xyxy.cpu().numpy() 
+
+	# Scores
+	scores = targets.boxes.conf.cpu().numpy()
+
+	# Filtration
+	threshold = 0.5
+	filtered_boxes = boxes[scores > threshold]
+	filtered_scores = scores[scores > threshold]
+
+	# Chose target
+	min_diff = float('inf')
+	points = []
+
+	for box, score in zip(filtered_boxes, filtered_scores):
+		x1, y1, x2, y2 = box
+		x = int( (x1 + x2) // 2 )
+		y = int( (y1 + y2) // 2 )
+
+		diff = abs(x - center_x) + abs(y - center_y)
+
+		if diff < min_diff:
+			min_diff = diff
+			points = [(x, y), (x1, y1), (x2, y2)]
+
+	return points
