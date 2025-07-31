353d51d0b90f0ff223d0992da56cd8ef072f1fef
Timurkaaa <timurzarovskiy@gmail.com>
2025-07-31 21:33:36 +0300

    feat(bot) create config
    AimLabs-Bot config

---
+++ config/config.py
@@ -0,0 +1,15 @@
+# OBS Virtual Cam
+FRAME_WIDTH = 424
+FRAME_HEIGHT = 240
+
+
+# Visualisation config
+VISUALISATION = True
+
+ALL_DETECTION_TARGETS_OVERLAY = True
+CURRENT_TARGET_OVERLAY = True
+FPS_OVERLAY = True
+
+# Visualisation colors. Format - BGR (Blue, Green, Red)
+FPS_COLOR = (0, 255, 0)
+CURRENT_TARGET_COLOR = (0, 0, 255)
