88a88a3cda9d573abd517d71ab37b4f0dca3f844
Timurkaaa <timurzarovskiy@gmail.com>
2025-08-02 21:43:33 +0300

    fix: update current_target.py

--- src/bot/modules/visualisation/current_target.py
+++ src/bot/modules/visualisation/current_target.py
@@ -3,9 +3,10 @@ import config.config as config
 
 def add_target(frame, target):
     height, width = frame.shape[:2]
+
     target = target[0]
 
-    cv2.line(frame, height, target[0], config.CURRENT_TARGET_COLOR)
-    cv2.line(frame, width, target[1], config.CURRENT_TARGET_COLOR)
+    cv2.line(frame, (height, target[0]), (0, target[0]), config.CURRENT_TARGET_COLOR)
+    cv2.line(frame, (width, target[1]), (0, target[1]), config.CURRENT_TARGET_COLOR)
     
     return frame
