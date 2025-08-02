49ab2fddde985fd70facdf2f3ea6e15aa392e721
Timurkaaa <timurzarovskiy@gmail.com>
2025-08-02 21:30:31 +0300

    feat: update model.py

--- src/bot/modules/model.py
+++ src/bot/modules/model.py
@@ -1,7 +1,8 @@
 import torch
 from ultralytics import YOLO
+import config.config as config
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(f'Using device: {device}')
 
-model = YOLO('models/0.1.0-alpha/model.pt').to(device)
+model = YOLO(f'models/{config.MODEL}/model.pt').to(device)
